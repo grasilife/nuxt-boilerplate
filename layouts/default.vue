b099786e63d5b26cf0983b510d2fd7b69dc1b412
grasilife <fu.guo.qiang@qq.com>
2020-03-04 20:59:36 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<!--
+ * @Author: 付国强
+ * @Date: 2020-01-11 01:00:04
+ * @LastEditors: 付国强
+ * @LastEditTime: 2020-01-11 01:00:05
+ * @Description:
+ -->
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  padding: 10px 30px;
+  border: 1px solid #3b8070;
+  border-radius: 4px;
+  color: #3b8070;
+  text-decoration: none;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  margin-left: 15px;
+  padding: 10px 30px;
+  border: 1px solid #35495e;
+  border-radius: 4px;
+  color: #35495e;
+  text-decoration: none;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<!--
+ * @Author: 付国强
+ * @Date: 2020-01-11 01:00:15
+ * @LastEditors  : 付国强
+ * @LastEditTime : 2020-01-11 01:00:22
+ * @Description:
+ -->
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        nuxt-app
+      </h1>
+      <h2 class="subtitle">
+        My badass Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  color: #35495e;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  padding-bottom: 15px;
+  color: #526488;
+  font-weight: 300;
+  font-size: 42px;
+  word-spacing: 5px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
